32efcf385e93be0c29c488aa241937e001c01aef
Idriss-rayan <rayanidriss27@gmail.com>
2025-01-18 02:42:01 +0600

    problem sove

--- lib/users/fragments/profile_fragment_screen.dart
+++ lib/users/fragments/profile_fragment_screen.dart
@@ -1,18 +1,93 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:untitled/users/userPreferences/current_user.dart';
 
 class ProfileFragmentScreen extends StatelessWidget
 {
+  final CurrentUser _currentUser = Get.put(CurrentUser());
 
+  Widget userInfoItemProfile(IconData iconData, String userData) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
+      ),
+      child: Row(
+        children: [
+          Icon(
+            iconData,
+            size: 30,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Text(
+            userData,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-            "Profile Fragment Screen"
+    return ListView(
+      padding: const EdgeInsets.all(32),
+      children: [
+        Center(
+          child: Image.asset(
+            "images/woman.png",
+            width: 240,
+          ),
         ),
-      ),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.person, _currentUser.user.user_name),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.email, _currentUser.user.user_email),
+        const SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: Material(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(8),
+            child: InkWell(
+              onTap: ()
+              {
+
+              },
+              borderRadius: BorderRadius.circular(32),
+              child: const Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 12,
+                ),
+                child: const Text(
+                  "Sign Out",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+      ],
     );
   }
 }
