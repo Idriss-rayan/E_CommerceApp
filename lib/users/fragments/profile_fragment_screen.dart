d468ae42c30040058ae3ad8e119743daba17285e
Idriss-rayan <rayanidriss27@gmail.com>
2025-01-18 17:04:42 +0600

    new day, new challenge

--- lib/users/fragments/profile_fragment_screen.dart
+++ lib/users/fragments/profile_fragment_screen.dart
@@ -8,6 +8,52 @@ class ProfileFragmentScreen extends StatelessWidget
 {
   final CurrentUser _currentUser = Get.put(CurrentUser());
 
+  signOutUser() async
+  {
+    var resultResponse = await Get.dialog(
+      AlertDialog(
+        backgroundColor: Colors.grey,
+        title: Text(
+          "Logout",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: const Text(
+          "Are you sure?\nyou wan to logout from app?"
+        ),
+        actions: [
+          TextButton(
+              onPressed: ()
+              {
+                Get.back();
+              },
+              child: const Text(
+                "No",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )
+          ),
+
+          TextButton(
+              onPressed: ()
+              {
+                Get.back(result: "loggedOut");
+              },
+              child: const Text(
+                "Yes",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )
+          ),
+        ],
+      )
+    );
+  }
+
   Widget userInfoItemProfile(IconData iconData, String userData) {
     return Container(
       decoration: BoxDecoration(
